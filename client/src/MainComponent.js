d10eaf49e3da36adc6775d0ae3f66ab6c8ada282
chrisgiem <krzysztofgiemza1@gmail.com>
2021-10-17 00:27:07 +0000

    add kropka dodaje tez podfoldery

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,12 +1,22 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import axios from "axios";
+import './MainComponent.css';
 
 const MainComponent = () => {
-    const getAllNumbers = useCallback(() => {});
+    const [values, setValues] = useState([]);
+
+    const getAllNumbers = useCallback(async () => {
+        const values = await axios.get('/api/values/all');
+        setValues(values);
+    });
 
     return (
         <div>
-            <button onClick={getAllNumbers}>Get all numbers</button>
+            <button onClick={getAllNumbers}>Get all numbers</button><br/>
+            <span className="title">Values</span>
+            <div className="values">
+                {values.map((value) => <div className="values">{value}</div>)}
+            </div>
         </div>
     );
 };
