538e14ef4fd69e89190dd86652af540e0106df4c
chrisgiem <krzysztofgiemza1@gmail.com>
2021-10-23 00:21:20 +0000

    dockerize1

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,22 +1,54 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import axios from "axios";
 import './MainComponent.css';
 
 const MainComponent = () => {
     const [values, setValues] = useState([]);
+    const [value, setValue] = useState('');
 
     const getAllNumbers = useCallback(async () => {
-        const values = await axios.get('/api/values/all');
-        setValues(values);
-    });
+        // we will use nginx to redirect it to the proper URL
+        const data = await axios.get("/api/values/all");
+        setValues(data.data.rows.map(row => row.number));
+    }, []);
+
+    const saveNumber = useCallback(
+        async event => {
+            event.preventDefault();
+
+            await axios.post('/api/values', {
+                value
+            });
+
+            setValue("");
+            getAllNumbers();
+        }.[value, getAllNumbers]
+    );
+
+    useEffect(() => {
+        getAllNumbers();
+    }, []);
 
     return (
         <div>
-            <button onClick={getAllNumbers}>Get all numbers</button><br/>
+            <button onClick={getAllNumbers}>Get all numbers</button>
+            <br />
             <span className="title">Values</span>
             <div className="values">
-                {values.map((value) => <div className="values">{value}</div>)}
+                {values.map(value => (
+                    <div className="values">{value}</div>
+                ))}
             </div>
+            <form className="form" onSubmit={saveNumber}>
+                <label>Enter your value: </label>
+                <input
+                    value={value}
+                    onChange={event => {
+                        setValue(event.target.value);
+                    }}
+                />
+                <button>Submit</button>
+            </form>
         </div>
     );
 };
